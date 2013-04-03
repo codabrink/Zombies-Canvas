bab0e652f25dbb27e4fb81cf33d8003e9eef3e0a
Dakota Brink <codabrink@gmail.com>
2013-04-03 16:49:36 -0400

    add some box stuff

---
+++ js/class/box.js
@@ -0,0 +1,40 @@
+function Box(x, y) {
+    var bConf = {
+        width: 100,
+        height: 100
+    },
+        bodies = [];
+
+    var fixDef = new b2FixtureDef();
+    var bodyDef = new b2BodyDef();
+
+    fixDef.shape = new b2PolygonShape();
+    fixDef.shape.SetAsBox(bConf['width'] / conf['scale'], 1 / conf['scale']);
+
+    bodyDef.type = b2Body.b2_staticBody;
+    bodyDef.position.Set(x / conf['scale'], y / conf['scale']);
+
+    //top
+    bodies.push(world.CreateBody(bodyDef));
+    bodies.last.SetSleepingAllowed(true);
+    bodies.last.CreateFixture(fixDef);
+
+    //bottom
+    bodyDef.position.Set(x / conf['scale'], (y + height) / conf['scale']);
+    bodies.push(world.CreateBody(bodyDef));
+    bodies.last.SetSleepingAllowed(true);
+    bodies.last.CreateFixture(fixDef);
+
+    //left
+    fixDef.shape.SetAsBox(1 / conf['scale'], bConf['height'] / conf['scale']);
+    bodyDef.position.Set(x / conf['scale'], y / conf['scale']);
+    bodies.push(world.CreateBody(bodyDef));
+    bodies.last.SetSleepingAllowed(true);
+    bodies.last.CreateFixture(fixDef);
+
+    //right
+    bodyDef.position.Set((x + bConf['width']) / conf['scale'], y / conf['scale']);
+    bodies.push(world.CreateBody(bodyDef));
+    bodies.last.SetSleepingAllowed(true);
+    bodies.last.CreateFixture(fixDef);
+}
